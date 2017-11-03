7645d7bfe2af249200ecd9c1eef500d89df0c4fd
IvanKovalov11 <32193366+IvanKovalov11@users.noreply.github.com>
2017-11-03 10:38:19 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+bool read(double *array, unsigned int size)
+{
+	string stroka;
+	getline(cin, stroka);
+	istringstream stream(stroka);
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		if (!(stream >> array[i]))
+		{
+			cout << "An error has occured while reading input data." << endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+
+bool proverka(double *arr, int n)
+{
+	int i, j;
+	for (i = 1; i < n; i++)
+	{
+		for (j = (i - 1); j >= 0; j--)
+			if (arr[j + 1] > arr[j])
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+	}
+}
+
+void insertionSort(double *arr, int n)
+{
+	int i, j;
+	for (i = 1; i < n ; i++)
+	{
+		for (j = (i - 1); j >= 0  ; j--)
+			if (arr[j + 1] < arr[j])
+			{
+				swap(arr[j], arr[j+1]);
+			}
+	}
+}
+
+void printArray(double arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << ' ';
+}
+
+int main()
+{
+	unsigned int size;
+	cin >> size;
+	cin.get();
+	double *arr = new double[size];
+	if (read(arr, size))
+	{
+		if (proverka(arr, size))
+		{
+			insertionSort(arr, size);
+			cout << "Sorted array: \n";
+			printArray(arr, size);
+		}
+		else
+		{
+			cout << "This line: ";
+			printArray(arr, size); 
+			cout << " is already sorted";
+		}
+	}
+	
+	cin.get();
+	return 0;
+}
